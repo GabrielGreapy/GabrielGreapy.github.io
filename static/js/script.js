b15d5039ea55557d4f5eacb9e461bf00d673d215
GabrielGreapy <159476031+GabrielGreapy@users.noreply.github.com>
2025-05-21 03:47:39 +0000

    Desisto de fazer no codespace

---
+++ static/js/script.js
@@ -0,0 +1,7 @@
+function sideBarOn() {
+    document.getElementById('sidebar').classList.add('active');
+}
+
+function sideBarOff() {
+    document.getElementById('sidebar').classList.remove('active');
+}
