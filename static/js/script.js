bfc8c32fe5c87a625c0e13564bda8aee6aa2ca51
Gabriel Martins <gabriel.martins.1@academico.ifpb.edu.br>
2025-05-21 02:07:28 -0300

    Sistema de dark e light mode

--- static/js/script.js
+++ static/js/script.js
@@ -5,3 +5,13 @@ function sideBarOn() {
 function sideBarOff() {
     document.getElementById('sidebar').classList.remove('active');
 }
+function alternarTema(){
+    const html = document.documentElement;
+    const tema = html.getAttribute("data-theme")
+    if(tema === "dark"){
+        html.setAttribute("data-theme", "light")
+    }
+    else{
+        html.setAttribute("data-theme", "dark")
+    }
+}
