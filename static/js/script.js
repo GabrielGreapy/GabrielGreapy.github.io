b8699b0024d2d7df3f2733d2bd77f6b401bb29ce
Gabriel Martins <gabriel.martins.1@academico.ifpb.edu.br>
2025-05-27 21:24:52 -0300

    main page terminada

--- static/js/script.js
+++ static/js/script.js
@@ -21,3 +21,31 @@ function abrirPopUp(){
 function fecharPopUp(){
     document.getElementById("popup").style.display = "none";
 }
+const slider = document.querySelectorAll('.slider');
+const btnPrev = document.getElementById("prev-button");
+const btnNext = document.getElementById("next-button");
+
+let currentSlide = 0;
+
+function hideSlider(){
+    slider.forEach(item => item.classList.remove('on'));
+}
+
+function showSlider(){
+    slider[currentSlide].classList.add('on');
+}
+
+function nextSlider(){
+    hideSlider();
+    currentSlide = (currentSlide === slider.length - 1) ? 0 : currentSlide + 1;
+    showSlider();
+}
+
+function prevSlider(){
+    hideSlider();
+    currentSlide = (currentSlide === 0) ? slider.length - 1 : currentSlide - 1;
+    showSlider();
+}
+
+btnNext.addEventListener("click", nextSlider);
+btnPrev.addEventListener("click", prevSlider);
